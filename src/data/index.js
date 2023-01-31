ce817b9a8dfe18a15d85e9af1deff6233b39da2a
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-31 13:21:16 +0100

    index js

---
+++ src/data/index.js
@@ -0,0 +1,52 @@
+const navlist = [
+    "home",
+    'about me',
+    'testimonials',
+    'my blog',
+    'meetups',
+    'shop',
+    'contact me'
+]
+
+const criticsArticles = [
+    {
+        'id': '1',
+        'title': 'Best Author of his Generation',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi rerum a qui enim vero provident ipsam, voluptatem accusamus ut. Praesentium commodi optio quasi. Nam ab veniam amet harum, eaque odio aliquam voluptatibus distinctio iure mollitia minus libero eius cum sint laboriosam deleniti exercitationem officia, qui magnam assumenda corporis. Illum, uno!',
+        'author': 'Justine Kilpatrick',
+        'daily': 'The New York Times'
+    },
+    {
+        'id': '2',
+        'title': 'a true masterpiece, bravo!',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi rerum a qui enim vero provident ipsam, voluptatem accusamus ut. Praesentium commodi optio quasi. Nam ab veniam amet harum, eaque odio aliquam voluptatibus distinctio iure mollitia minus libero eius cum sint laboriosam deleniti exercitationem officia, qui magnam assumenda corporis. Illum, due!',
+        'author': 'Gerald Hendley',
+        'daily': 'the guardian'
+    },
+    {
+        'id': '3',
+        'title': 'a unique view on the world',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi rerum a qui enim vero provident ipsam, voluptatem accusamus ut. Praesentium commodi optio quasi. Nam ab veniam amet harum, eaque odio aliquam voluptatibus distinctio iure mollitia minus libero eius cum sint laboriosam deleniti exercitationem officia, qui magnam assumenda corporis. Illum, tre!',
+        'author': 'Mark Maxey',
+        'daily': 'the globe and mail'
+    }
+]
+
+const eventsArticles = [
+    {
+        'id': '1',
+        'title': 'chicago book signing',
+        'text': 'Lorem ipsum dolor sit amet, voluptatibus distinctio iure mollitia minus libero eius cum sint laboriosam deleniti exercitationem officia, qui magnam assumenda corporis. Illum, uno!',
+    },
+    {
+        'id': '2',
+        'title': 'meet & greet with amanda',
+        'text': 'Lorem ipsum dolor sit amet, voluptatibus distinctio iure mollitia minus libero eius cum sint laboriosam deleniti exercitationem officia, qui magnam assumenda corporis. Illum, uno!',
+    }, {
+        'id': '3',
+        'title': 'open book: dialogues',
+        'text': 'Lorem ipsum dolor sit amet, voluptatibus distinctio iure mollitia minus libero eius cum sint laboriosam deleniti exercitationem officia, qui magnam assumenda corporis. Illum, uno!',
+    }
+]
+
+export { navlist, criticsArticles, eventsArticles }
