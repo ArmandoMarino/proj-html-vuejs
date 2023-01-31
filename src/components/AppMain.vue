46ceb86473723bd1c105a857aef72b5c5d844097
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-31 18:11:48 +0100

    about component neutro

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,29 @@
 <script>
 import AppHero from './AppHero.vue';
+import about from './sub_components/about.vue';
 export default {
     name: 'AppMain',
     components: {
-        AppHero
+        AppHero,
+        about
+    },
+    data() {
+        return {
+            about:
+            {
+                "title": 'more about damon vaughn',
+                "text": 'Damon Vaughn is a professor of psychology at the University of Toronto, a clinical psychologis and the author of the multi- milion copy bestseller "The Story of my life."'
+            }
+        }
     }
 }
 </script>
 
 <template>
-    <app-hero></app-hero>
+    <main>
+        <app-hero></app-hero>
+        <about :title="this.about.title" :text="this.about.text"></about>
+    </main>
 </template>
 
 <style>
---
+++ src/components/sub_components/about.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'about',
+    props: {
+        title: String,
+        text: String
+    }
+}
+</script>
+
+<template>
+    <div id="about" class="text-center d-flex">
+        <h1>{{ title }}</h1>
+        <h3>{{ text }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
