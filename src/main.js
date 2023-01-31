21ce221eaf2d7f6ec5743837b8e3856f27f91ed5
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-31 11:13:20 +0100

    101 Node - SASS - Bootstrap

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './style.css'     JUST IN CASE
+import App from './App.vue'
+
+createApp(App).mount('#app')
