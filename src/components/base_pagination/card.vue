2accad99e935740690aa7e9483df0de6329fb810
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-01 10:33:07 +0100

    card and section critics alpha

---
+++ src/components/base_pagination/card.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'card',
+    props: {
+        title: String,
+        class: String,
+        text: String,
+        image: String,
+        alt: String,
+        daily: String,
+        author: String
+    }
+}
+</script>
+
+
+<template>
+    <div :class="class" class="card p-2">
+        <h2>{{ title }}</h2>
+        <hr>
+        <p>{{ text }}</p>
+        <div class="d-flex justify-content-center align-item-center">
+            <img :src="image" :alt="alt">
+            <p>{{ author }},</p>
+            <p :class="class">{{ daily }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/sub_components/critics.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'critics'
+}
+</script>
+
+<template>
+    <div class="say">
+        <div>
+            <h1>What th Critics Say</h1>
+            <p>TESTIMONIALS FROM AROUND THE WORLD</p>
+        </div>
+        <button>CIAO</button>
+    </div>
+    <!-- CARDS -->
+    <div class="container d-flex">
+        <div class="left">
+
+        </div>
+        <div class="right">
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.left {
+    flex-basis: 60%;
+}
+
+.right {
+    flex-basis: 40%;
+}
+</style>
