b29e7e0a08ae934633c3ece2ffbd8d017dfb4556
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-02 16:36:36 +0100

    new navbar

--- src/components/neutro_components/navBar.vue
+++ src/components/neutro_components/navBar.vue
@@ -10,38 +10,50 @@ export default {
 </script>
 
 <template>
-    <nav>
-        <ul class="d-flex justify-content-between">
-            <li v-for="list in lists"><a href="">{{ list }}</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-lg bg-trasparent">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler justify-content-between" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarNavDropdown">
+                <ul class="navbar-nav justify-content-between">
+                    <li v-for="list in lists" class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">{{ list }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </template>
+<!-- <li v-for="list in lists"><a href="">{{ list }}</a></li> -->
 
 <style lang="scss" scoped>
 @use '../../assets/scss/partials/variables' as*;
 
-nav {
 
-    ul {
-        list-style-type: none;
-        display: flex;
-        padding: 0;
-        margin: 0;
 
-        li {
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+
+    li {
+        padding: 5px;
+        text-transform: capitalize;
+        font-weight: 600;
+        border-top: 2px solid transparent;
+
+        a {
+            text-shadow: 2px 3px 5px rgba(0, 0, 0, 0.5);
             padding: 5px;
-            text-transform: capitalize;
-            font-weight: 600;
-            border-top: 2px solid transparent;
+        }
 
-            a {
-                text-shadow: 2px 3px 5px rgba(0, 0, 0, 0.5);
-                padding: 5px;
-            }
-
-            li:hover {
-                border-top: 2px solid orange;
-            }
+        li:hover {
+            border-top: 2px solid orange;
         }
     }
 }
--- src/components/sub_components/audioBook.vue
+++ src/components/sub_components/audioBook.vue
@@ -24,24 +24,24 @@ export default {
     <div id="audio-book" class="pt-5">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <div class="d-flex justify-content-center">
-                        <!-- LEFT -->
-                        <img class="app" src="../../assets/img/audible-app-2x.png" alt="audible">
+                <div class="col-4 offset-2">
+                    <!-- LEFT -->
+                    <img class="img-fluid" src="../../assets/img/audible-app-2x.png" alt="audible">
+                </div>
 
-                        <!-- RIGHT -->
-                        <div class="r-audio-book d-flex justify-content-center flex-column text-center">
-                            <h2>New Audiobook</h2>
-                            <h6>NO TIME TO READ? NO PROBLEM. LISTEN TO IT</h6>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                                Obcaecati amet esse consequuntur.
-                            </p>
-                            <!-- SUB-BOTTOM -->
-                            <div class="b-audio-book d-flex justify-content-center">
-                                <img class="img-fluid" src="../../assets/img/audible-logo.png" alt="audible">
-                                <img class="img-fluid" src="../../assets/img/bn-logo-tall.png" alt="bn">
-                                <img class="img-fluid" src="../../assets/img/kindle-logo-tall.png" alt="kindle">
-                            </div>
+                <div class="col-4 d-flex justify-content-center flex-column text-center">
+                    <!-- RIGHT -->
+                    <div class="r-audio-book ">
+                        <h2>New Audiobook</h2>
+                        <h6>NO TIME TO READ? NO PROBLEM. LISTEN TO IT</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Obcaecati amet esse consequuntur.
+                        </p>
+                        <!-- SUB-BOTTOM -->
+                        <div class="b-audio-book d-flex justify-content-center">
+                            <img class="img-fluid" src="../../assets/img/audible-logo.png" alt="audible">
+                            <img class="img-fluid" src="../../assets/img/bn-logo-tall.png" alt="bn">
+                            <img class="img-fluid" src="../../assets/img/kindle-logo-tall.png" alt="kindle">
                         </div>
                     </div>
                 </div>
@@ -59,8 +59,8 @@ export default {
     background-color: $pm-cream;
 
     img {
-        width: 20%;
-        padding: 2rem;
+        width: 15%;
+        padding: 1rem;
     }
 }
 
@@ -80,12 +80,7 @@ export default {
     .b-audio-book {
         img {
             padding: 10px;
-            height: 90%;
         }
-    }
-
-    .app {
-        width: 30%;
     }
 }
 </style>
