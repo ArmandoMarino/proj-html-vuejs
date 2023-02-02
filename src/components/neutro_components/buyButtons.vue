5adeea415dad67e637a029c8850d239c016e75a4
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-02 13:24:16 +0100

    fontawesome

--- src/components/neutro_components/buyButtons.vue
+++ src/components/neutro_components/buyButtons.vue
@@ -13,22 +13,39 @@ export default {
 
 <template>
     <!-- BUTTON LEFT -->
-    <button class="buy-btn-l" name="buy-button" type="submit">
-        <span>{{ imageAgencyLeft }}</span>
-        {{ placeHolderLeft }}
-    </button>
-    <!-- BUTTON RIGHT -->
-    <button class="buy-btn-r" name="buy-button" type="submit">
-        {{ placeHolderRight }}
-    </button>
+    <div class="buy-btn">
+        <button class="buy-btn-l" name="buy-button" type="submit">
+            <span>{{ imageAgencyLeft }}</span>
+            {{ placeHolderLeft }}
+        </button>
+        <!-- BUTTON RIGHT -->
+        <button class="buy-btn-r" name="buy-button" type="submit">
+            {{ placeHolderRight }}
+        </button>
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as *;
+
+.wrapper {
+    font-weight: 700;
+}
+
+.buy-btn>* {
+    background-color: #f5fcfa;
+    border-radius: 5px;
+    font-size: 15px;
+    padding: 5px;
+}
+
 .buy-btn {
     display: flex;
 
     img {
         width: 20px;
     }
+
 }
 </style>
