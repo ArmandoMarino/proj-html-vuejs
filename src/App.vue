fc265e2a0247c1f15026c0d734be28e10ed7259a
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-03 11:04:00 +0100

    restore

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ export default {
     AppHeader,
     AppMain,
     AppFooter
-  }
+  },
 }
 
 </script>
@@ -22,28 +22,6 @@ export default {
 @use './assets/scss/partials/variables' as *;
 // font-family: 'Cedarville Cursive', cursive;  HERO CURSIVE
 // font-family: 'Newsreader', serif;   PRIMARY FONT
-
-// TODO LOADER WITH DATA 
-.loader {
-  border: 16px solid #f3f3f3;
-  /* Light grey */
-  border-top: 16px solid #3498db;
-  /* Blue */
-  border-radius: 50%;
-  width: 120px;
-  height: 120px;
-  animation: spin 2s linear infinite;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-
-  100% {
-    transform: rotate(360deg);
-  }
-}
 
 a {
   text-decoration: none;
